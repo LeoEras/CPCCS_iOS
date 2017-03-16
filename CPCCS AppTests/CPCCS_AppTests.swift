cd756e1817fd646036c5cb0ed46d6fb558733456
Leonardo <leonardo@Leonardos-iMac.local>
2017-03-15 20:14:11 -0500

    Initial Commit

---
+++ CPCCS AppTests/CPCCS_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CPCCS_AppTests.swift
+//  CPCCS AppTests
+//
+//  Created by Leonardo on 3/15/17.
+//  Copyright © 2017 CPCCS. All rights reserved.
+//
+
+import XCTest
+@testable import CPCCS_App
+
+class CPCCS_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
