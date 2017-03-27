2c7b5e1a807c1d8ebe1bc0deae5156c4ff833529
Leonardo <leonardo@Leonardos-iMac.local>
2017-03-27 17:11:09 -0500

    Primer commit

--- CPCCS App/ViewController.swift
+++ CPCCS App/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuView: UIView!
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
 
+    var menuShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any) {
+        if (menuShowing){
+            leadingConstraint.constant = -260
+        } else {
+            leadingConstraint.constant = -55
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        menuShowing = !menuShowing
     }
-
-
 }
 
