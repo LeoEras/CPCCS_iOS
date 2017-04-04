cdde8cd7a55695f0dd0fe666e47e111976b24d5f
Leonardo <leonardo@Leonardos-iMac.local>
2017-04-03 21:50:14 -0500

    Correcciones en Page view Controller

--- CPCCS App/DenunciasViewController.swift
+++ CPCCS App/DenunciasViewController.swift
@@ -5,19 +5,18 @@
 //  Created by Leonardo on 4/2/17.
 //  Copyright © 2017 CPCCS. All rights reserved.
 //
-
 import UIKit
 
 class DenunciasViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
-    let pages = ["Denuncias2", "Denuncias3", "Denuncias1", "Denuncias3"]
+    let pages = ["Denuncias1", "Denuncias2", "Denuncias3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
         self.dataSource = self
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "Denuncias1")
-        setViewControllers([vc!], // Has to be a single item array, unless you're doing double sided stuff I believe
+        setViewControllers([vc!],
             direction: .forward,
             animated: true,
             completion: nil)
@@ -28,7 +27,8 @@ class DenunciasViewController: UIPageViewController, UIPageViewControllerDelegat
         for view in view.subviews {
             if view is UIScrollView {
                 view.frame = UIScreen.main.bounds // Why? I don't know.
-            } else if view is UIPageControl {
+            }
+            else if view is UIPageControl {
                 view.backgroundColor = UIColor.black
             }
         }
@@ -37,8 +37,6 @@ class DenunciasViewController: UIPageViewController, UIPageViewControllerDelegat
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let identifier = viewController.restorationIdentifier {
             if let index = pages.index(of: identifier) {
-                print("3era funcion")
-                print(index)
                 if index > 0 {
                     return self.storyboard?.instantiateViewController(withIdentifier: pages[index-1])
                 }
@@ -50,8 +48,6 @@ class DenunciasViewController: UIPageViewController, UIPageViewControllerDelegat
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let identifier = viewController.restorationIdentifier {
             if let index = pages.index(of: identifier) {
-                print("4ta Funcion")
-                print(index)
                 if index < pages.count - 1 {
                     return self.storyboard?.instantiateViewController(withIdentifier: pages[index+1])
                 }
@@ -67,8 +63,6 @@ class DenunciasViewController: UIPageViewController, UIPageViewControllerDelegat
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         if let identifier = viewControllers?.first?.restorationIdentifier {
             if let index = pages.index(of: identifier) {
-                print("Last")
-                print(index)
                 return index
             }
         }
